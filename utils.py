b2eab00d986b6f6f476f976946f415438efdf348
Sam O'Neal <sam.oneal@gmail.com>
2024-02-06 20:26:16 -0800

    Users page formatting.

--- utils.py
+++ utils.py
@@ -2,31 +2,32 @@ import json
 
 
 def add_stars(rating, max=5):
-  full = round(rating)
-  half = False
-  if 0.75 > rating % 1 > 0.25:
-    half = True
-    full -= 1
-  result = []
-  for i in range(full):
-    result.append('<span class="fa fa-star checked"></span>')
-  if half:
-    result.append('<span class="fa fa-star-half checked"></span>')
-  while len(result) < max:
-    result.append('<span class="fa fa-star unchecked"></span>')
-  return "".join(result)
+    full = round(rating)
+    half = False
+    if 0.75 > rating % 1 > 0.25:
+        half = True
+        full -= 1
+    result = []
+    for i in range(full):
+        result.append('<span class="fa fa-star checked"></span>')
+    if half:
+        result.append('<span class="fa fa-star-half checked"></span>')
+    while len(result) < max:
+        result.append('<span class="fa fa-star unchecked"></span>')
+    return "".join(result)
 
 
 def movie_stars(movie_dict):
-  for item in movie_dict:
-    item['stars'] = add_stars(item['rating'])
-  return movie_dict
+    for item in movie_dict:
+        item["stars"] = add_stars(item["rating"])
+    return movie_dict
 
 
 def load_user_data(user_number):
-  # Read project data from JSON file
-  with open('test.json') as json_file:
-    for user in json.load(json_file):
-      if user['id'] == user_number:
-        return user
-    return None
+    # Read project data from JSON file
+    with open("test.json") as json_file:
+        users = json.load(json_file)
+        for user in users:
+            if user["id"] == user_number:
+                return user
+        return None
